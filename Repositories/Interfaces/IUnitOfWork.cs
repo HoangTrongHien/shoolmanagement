7722340c5e7f1b0e5a8dda42876744eda2744f04
Hien.Hoang <hien.hoang@mainbridgehp.com>
2025-04-28 16:27:11 +0700

    Update: unitOfwork

--- Repositories/Interfaces/IUnitOfWork.cs
+++ Repositories/Interfaces/IUnitOfWork.cs
@@ -4,5 +4,11 @@ public interface IUnitOfWork : IDisposable
 {
     IStudentRepository Students { get; }
     ITeacherRepository Teachers { get; }
+    ISubjectRepository Subjects { get; }
+    ISubscriptionRepository Subscriptions { get; }
+    IScheduleInformationRepository ScheduleInformations { get; }
+    IOnTeachClassRepository OnTeachClasses { get; }
+    IAttendanceRepository Attendances { get; }
+
     Task<int> CompleteAsync();
 }
--- Repositories/UnitOfWork.cs
+++ Repositories/UnitOfWork.cs
@@ -9,12 +9,23 @@ public class UnitOfWork : IUnitOfWork
 
     public IStudentRepository Students { get; }
     public ITeacherRepository Teachers { get; }
+    public IAttendanceRepository Attendances { get; }
+    public ISubjectRepository Subjects { get; }
+    public IOnTeachClassRepository OnTeachClasses { get; }
+    public ISubscriptionRepository Subscriptions { get; }
+    public IScheduleInformationRepository ScheduleInformations { get; }
+
 
     public UnitOfWork(AppDbContext dbContext)
     {
         _dbContext = dbContext;
         Students = new StudentRepository(_dbContext);
         Teachers = new TeacherRepository(_dbContext);
+        Attendances = new AttendanceRepository(_dbContext);
+        Subjects = new SubjectRepository(_dbContext);
+        OnTeachClasses = new OnTeachClassRepository(_dbContext);
+        Subscriptions = new SubscriptionRepository(_dbContext);
+        ScheduleInformations = new ScheduleInformationRepository(_dbContext);
     }
 
     public async Task<int> CompleteAsync() => await _dbContext.SaveChangesAsync();
